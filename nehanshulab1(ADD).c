8d05af1a0bb32e4cbaa4885ffc20e5b9897a906a
Nehanshu1 <chauhannehanshu13@gmail.com>
2025-08-08 10:54:16 +0530

    Add files via upload

---
+++ nehanshulab1(ADD).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main() {
+int num1,num2,sum;
+
+printf("first number :");
+scanf("%d", &num1);
+
+printf("second number :");
+scanf("%d", &num2);
+
+sum= num1+num2;
+
+printf("this is my sum : %d", sum);
+
+return 0;
+
+}
